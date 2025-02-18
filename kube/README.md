8a4db02e8895c94137fc6e330b2c75eff4965555
immauss <scott@immauss.com>
2025-02-18 15:15:40 +0100

    not really working yet.

---
+++ kube/README.md
@@ -0,0 +1,4 @@
+ToDos:
+- how to get CoreConfig consistent and into container
+- update Coreconfig to point to actual database
+- get it to actually run
--- kube/create-pvc.sh
+++ kube/create-pvc.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Define local storage directory
-LOCAL_STORAGE_DIR="/home/scott/Projects/TAK/kube/local-storage"
+LOCAL_STORAGE_DIR="/Users/scott/Projects/tak-utils/kube/local-storage"
 
 # Create the local storage directory if it doesn't exist
 if [ ! -d "$LOCAL_STORAGE_DIR" ]; then
-    mkdir -p $LOCAL_STORAGE_DIR
+    mkdir -p $LOCAL_STORAGE_DIR/db
+    mkdir -p $LOCAL_STORAGE_DIR/data
     echo "Created local storage directory: $LOCAL_STORAGE_DIR"
 else
     echo "Local storage directory already exists: $LOCAL_STORAGE_DIR"
 fi
 
 # Apply Persistent Volume and Persistent Volume Claim
-kubectl apply -f deployment.yaml
+TMP_YAML=$(mktemp)
+sed -e "s|STORAGEPATH|$LOCAL_STORAGE_DIR|" storage.yaml > $TMP_YAML
+kubectl apply -f $TMP_YAML
+rm $TMP_YAML
 
 # Verify PV and PVC status
 kubectl get pv
