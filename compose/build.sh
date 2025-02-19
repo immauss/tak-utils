42602271afa779d1ad378817e0120d0fd5bce672
immauss <scott@immauss.com>
2025-02-19 18:51:03 +0100

    added second option to build.sh

--- compose/build.sh
+++ compose/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    echo "Usage: $0 <version>"
+    echo "Usage: $0 <version> <push|load>"
     exit 1
 fi
 tag=$1
-docker buildx build -t immauss/tak-tak:$tag -f Dockerfile.tak . --load
-docker buildx build -t immauss/tak-db:$tag -f Dockerfile.db . --load
+docker buildx build -t immauss/tak-tak:$tag -f Dockerfile.tak . --$2
+docker buildx build -t immauss/tak-db:$tag -f Dockerfile.db . --$2
