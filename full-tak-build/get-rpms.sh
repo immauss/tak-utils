cc1f9be63acc08482b43a993feb6440a91fcb767
GE Scott Knauss <scott@immauss.com>
2025-03-10 09:56:00 +0000

    script to pull needed rpms

---
+++ full-tak-build/get-rpms.sh
@@ -0,0 +1,3 @@
+wget https://download.postgresql.org/pub/repos/yum/15/redhat/rhel-8-x86_64/postgresql15-15.12-1PGDG.rhel8.x86_64.rpm
+wget https://download.postgresql.org/pub/repos/yum/15/redhat/rhel-8-x86_64/postgresql15-libs-15.12-1PGDG.rhel8.x86_64.rpm
+
